35f50edffaf8dc26a9d6798c1809189ead7506d0
clintonhays <clintonhays84@gmail.com>
2020-12-05 17:15:43 +0900

    create showPage function

--- js/script.js
+++ js/script.js
@@ -3,28 +3,43 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+/**
+ * Global Variables 
+ **/
 
+const classList = document.querySelector('ul.student-list');
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+const showPage = (list, page) => {
+  const firstIndex = page * 9 - 9;
+  const finalIndex = page * 9;
+  ul.innerHtml = '';
 
-
+  list.forEach((item, index) => {
+    if (index >= firstIndex && index < finalIndex) {
+      const studentItem = `
+         <li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src=${item.picture.large} alt="Profile Picture">
+            <h3>${item.name.first} ${item.name.last}</h3>
+            <span class="email">${item.email}</span>
+         </div>
+         <div class="joined-details">
+            <span class="date">Joined ${item.registered.length}</span>
+         </div>
+         </li>
+        `;
+    }
+  });
+};
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-
-
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
-
 
 // Call functions
